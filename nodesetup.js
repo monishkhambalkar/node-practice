18f098ddf078a39c483e6177678613521354e1fa
Monish Khambalkar <monishkhambalkar@gmail.com>
2024-11-24 23:21:30 +0530

    add practice task

---
+++ nodesetup.js
@@ -0,0 +1,28 @@
+// npm init -y
+
+// npm install express
+
+// npm install --save-dev nodemon
+
+//touch index.js
+
+//const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, Node.js!');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+// package.json;scripts
+// "scripts": {
+//     "start": "node index.js",
+//     "dev": "nodemon index.js"
+// }
+
+//npm run start
+//npm run dev
